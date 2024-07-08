3700de3d70a58f3987eb0f9c347a1ecae13eb491
akbarjon <akbarjonsharifjonov33@gmail.com>
2024-07-08 11:46:32 +0500

    sayt

--- script/app.js
+++ script/app.js
@@ -1,5 +1,8 @@
 const sidebar = document.querySelector(".sidebar")
 const items = document.querySelector(".items")
+const button__div = document.querySelector(".button__div")
+const button = document.querySelector(".button")
+
 
 
 items.addEventListener("click", function(){
@@ -9,4 +12,15 @@ items.addEventListener("click", function(){
 
 function sidebarOpen(){
     sidebar.classList.toggle("show")
+}
+
+
+
+button.addEventListener("click", function(){
+    buttonOpen()
+})
+
+
+function buttonOpen(){
+    button__div.classList.toggle("but")
 }
